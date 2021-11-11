6c2b177a087850148d60e67f219b6620a2bf0ccd
Nazar <rohovsky@ucu.edu.ua>
2021-11-11 22:10:13 +0200

    #4 Implemented basic AssetType structure

--- airport/airport-pojo-bl/src/main/java/helsinki/assets/AssetType.java
+++ airport/airport-pojo-bl/src/main/java/helsinki/assets/AssetType.java
@@ -2,7 +2,7 @@ package helsinki.assets;
 
 import ua.com.fielden.platform.entity.DynamicEntityKey;
 import helsinki.common.validators.NoSpacesValidator;
-import ua.com.fielden.platform.entity.AbstractEntity;
+import helsinki.personnel.Person;
 import ua.com.fielden.platform.entity.ActivatableAbstractEntity;
 import ua.com.fielden.platform.entity.annotation.KeyType;
 import ua.com.fielden.platform.entity.annotation.KeyTitle;
@@ -11,6 +11,7 @@ import ua.com.fielden.platform.entity.annotation.CompositeKeyMember;
 import ua.com.fielden.platform.entity.annotation.MapEntityTo;
 import ua.com.fielden.platform.entity.annotation.MapTo;
 import ua.com.fielden.platform.entity.annotation.Observable;
+import ua.com.fielden.platform.entity.annotation.Required;
 import ua.com.fielden.platform.entity.annotation.Title;
 import ua.com.fielden.platform.entity.annotation.mutator.BeforeChange;
 import ua.com.fielden.platform.entity.annotation.mutator.Handler;
@@ -24,34 +25,40 @@ import ua.com.fielden.platform.reflection.TitlesDescsGetter;
 import ua.com.fielden.platform.utils.Pair;
 
 /**
- * An entity for classification of assets.
+ * An entity for typisation of assets.
  *
  * @author Developers
  *
  */
 @KeyType(DynamicEntityKey.class)
-@KeyTitle("Asset Class")
-@CompanionObject(AssetClassCo.class)
+@KeyTitle("Asset Type")
+@CompanionObject(AssetTypeCo.class)
 @MapEntityTo
 @DescTitle("Description")
 @DisplayDescription
 @DescRequired
-@EntityTitle(value = "Asset Class", desc = "Used for classification of assets.")
-public class AssetClass extends ActivatableAbstractEntity<DynamicEntityKey> {
+@EntityTitle(value = "AssetType", desc = "Used for typsation of assets.")
+public class AssetType extends ActivatableAbstractEntity<DynamicEntityKey> {
 
-    private static final Pair<String, String> entityTitleAndDesc = TitlesDescsGetter.getEntityTitleAndDesc(AssetClass.class);
+    private static final Pair<String, String> entityTitleAndDesc = TitlesDescsGetter.getEntityTitleAndDesc(AssetType.class);
     public static final String ENTITY_TITLE = entityTitleAndDesc.getKey();
     public static final String ENTITY_DESC = entityTitleAndDesc.getValue();
-
-    @IsProperty(length = 50)
+    
+    @IsProperty
     @MapTo
-    @Title(value = "Name", desc = "A unique asset class name.")
+    @Title(value = "Name", desc = "A unique Asset Type name.")
     @CompositeKeyMember(1)
     @BeforeChange({@Handler(MaxLengthValidator.class), @Handler(NoSpacesValidator.class)})
     private String name;
+    
+    @IsProperty
+    @MapTo
+    @Required
+    @Title(value = "Asset class", desc = "Represents the asset class of an asset type.")
+    private AssetClass assetClass;
 
     @Observable
-    public AssetClass setName(final String name) {
+    public AssetType setName(final String name) {
         this.name = name;
         return this;
     }
@@ -61,16 +68,29 @@ public class AssetClass extends ActivatableAbstractEntity<DynamicEntityKey> {
     }
     
     @Observable
+    public AssetType setAssetClass(final AssetClass assetClass) {
+        this.assetClass = assetClass;
+        return this;
+    }
+
+    public AssetClass getAssetClass() {
+        return assetClass;
+    }
+    
+    
+    @Observable
     @Override
-        public AssetClass setDesc(String desc) {
+        public AssetType setDesc(String desc) {
             super.setDesc(desc);
             return this;
         }
-    
-@Override
-@Observable
-    public AssetClass setActive(boolean active) {
-        super.setActive(active);
-        return this;
-    }
+
+    @Override
+    @Observable
+        public AssetType setActive(boolean active) {
+            super.setActive(active);
+            return this;
+        }
+
+
 }
